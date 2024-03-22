6e77a93c9d17161181283a45d22ae579e58edb57
ghollisjr <ghollisjr@gmail.com>
2024-03-21 23:10:44 -0400

    Added deletion example

--- example/insertion-deletion.lisp
+++ example/insertion-deletion.lisp
@@ -9,3 +9,19 @@
                    (list "this is" 3)
                    (list "more data" 4))
     table))
+
+(defun deletion-example ()
+  (let ((table (make-table '(("hello" 1)
+                             ("world" 2)
+                             ("this is" 3)
+                             ("more data" 4)
+                             ("to ponder" 5))
+                           :fields '("message" "index"))))
+    ;; can delete a specific row by index
+    (table-delete! table 1) ; -> ("world" 2) is gone
+    ;; or a list of rows by indices
+    (table-delete! table (list 2 3)) ; -> last 2 rows gone
+    ;; or using a boolean function
+    (table-delete! table
+                   (lambda (message index)
+                     (> index 2))))) ; -> only ("hello" 1) remaining
