fca38a398bdb3a0f8fb8352afef9160762ae9458
ghollisjr <ghollisjr@gmail.com>
2024-03-25 22:26:33 -0400

    Added comment to aggregation example

--- example/aggregation.lisp
+++ example/aggregation.lisp
@@ -5,8 +5,11 @@
   "Example aggregation: sum x grouped by y"
   (let ((table (make-table '((1 2) (3 4) (5 4)) :field-names '("x" "y"))))
     (aggregate (with-aggregation gy ((sumx (agg-sum gy)))
-                   (&optional x y)
                    (list gy (sumx))
+                   ;; Could also do:
+                   ;; (list gy sumx)
+                   ;; using the convenient symbol-macrolet
+                   (&optional x y)
                  (declare (ignore y))
                  (sumx x))
                table
