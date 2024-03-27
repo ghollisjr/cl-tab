795259d79f5e52b77128bc94bd67a09ee9ac7214
ghollisjr <ghollisjr@gmail.com>
2024-03-26 20:50:03 -0400

    Added SQL output and example

--- example/postgresql.lisp
+++ example/postgresql.lisp
@@ -1,8 +1,24 @@
 (require 'cl-tab)
 (in-package :tb)
 
-(defun psql-example ()
+(defun example-psql ()
+  "Read table from PostgreSQL"
   (clsql:connect '("localhost" "test" "test" "password")
                  :database-type :postgresql)
   ;; get table content:
   (query "select * from some_table;"))
+
+(defun example-psql2 ()
+  "Write table to PostgreSQL"
+  (clsql:connect '("localhost" "some_database" "user" "password")
+                 :database-type :postgresql)
+  ;; send table content to PostgreSQL server in a table:
+  (map nil
+       #'clsql:execute-command
+       (table->sql
+        (make-table '((1 2)
+                      (3 4))
+                    :field-names '("x" "y"))
+        "some_database.public.test"
+        :batch-size 10))
+  (clsql:disconnect))
--- sql.lisp
+++ sql.lisp
@@ -8,3 +8,75 @@
       (clsql:query query-expression
                    :database database)
     (make-table results :field-names columns)))
+
+(defun sql-type (obj)
+  (typecase obj
+    (character "CHAR")
+    (string "VARCHAR(65535)")
+    (symbol "VARCHAR(65535)")
+    (integer "INTEGER")
+    (float "FLOAT")
+    ;; default to strings
+    (t "VARCHAR(65535)")))
+
+(defun sql-convert (obj)
+  (typecase obj
+    (character (format nil "'~a'" obj))
+    (string (format nil "'~a'" obj))
+    (null "null")
+    (symbol (format nil "'~a'" obj))
+    (integer (format nil "~a" obj))
+    (float (format nil "~f" obj))
+    (t (format nil "'~a'" obj))))
+
+(defun table-sql-types (table)
+  "Tries to infer SQL types from table"
+  (mapcar (lambda (d)
+            (sql-type (aref d 0)))
+          (data table)))
+
+(defgeneric table->sql (table name
+                        &key
+                          db-type
+                          types
+                          batch-size
+                        &allow-other-keys)
+  (:documentation "Generates a list of SQL queries to create the table and insert data.
+
+* db-type (CURRENTLY UNUSED) controls type of db server,
+  e.g. :postgresql.
+
+* types is an optional list of SQL types for the columns.  If not
+  supplied, they will be inferred via #'table-sql-types.
+
+* batch-size controls the number of rows to attempt to insert in a
+  single insert statement.")
+  (:method ((table table) name &key db-type types (batch-size 1) &allow-other-keys )
+    (declare (ignore db-type))
+    (let ((types (or types (table-sql-types table))))
+      (list* (format nil
+                     "create table ~a (~{~a ~a~^, ~});"
+                     name
+                     (apply #'append
+                            (mapcar #'list
+                                    (field-names table)
+                                    types)))
+             (let ((nbatches (ceiling (table-length table)
+                                      batch-size)))
+               (loop
+                 for i below nbatches
+                 collect
+                 (let* ((batch
+                          (loop
+                            for j from (* i batch-size)
+                              below (min (table-length table)
+                                         (* (1+ i) batch-size))
+                            collect (table-ref table j)))
+                        (vstrings
+                          (mapcar (lambda (row)
+                                    (format nil "(~{~a~^, ~})"
+                                            row))
+                                  batch)))
+                   (format nil "insert into ~a values ~{~a~^, ~};"
+                           name
+                           vstrings))))))))
