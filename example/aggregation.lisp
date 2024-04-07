b3252743b93b5ff48262120786086f165f12a7a2
ghollisjr <ghollisjr@gmail.com>
2024-04-07 01:56:54 -0400

    Using new shorthand

--- example/aggregation.lisp
+++ example/aggregation.lisp
@@ -4,15 +4,15 @@
 (defun example-aggregate ()
   "Example aggregation: sum x grouped by y"
   (let ((table (make-table '((1 2) (3 4) (5 4)) :field-names '("X" "Y"))))
-    (aggregate (with-aggregation gy ((sumx (agg-sum gy)))
-                   (list gy (sumx))
-                   ;; Could also do:
-                   ;; (list gy sumx)
-                   ;; using the convenient symbol-macrolet
-                   (x)
-                 (sumx x))
-               table
-               ;; group by y
-               :group-fn (tlambda (y)
-                           y)
-               :field-names '("Y" "SUM(X)"))))
+    (agg (with-agg gy ((sumx (agg-sum gy)))
+             (list gy (sumx))
+             ;; Could also do:
+             ;; (list gy sumx)
+             ;; using the convenient symbol-macrolet
+             (x)
+           (sumx x))
+         table
+         ;; group by y
+         :group-fn (tlambda (y)
+                     y)
+         :field-names '("Y" "SUM(X)"))))
