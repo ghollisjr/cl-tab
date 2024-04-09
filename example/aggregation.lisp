41e589d3f8c4e57f4ccec15a83b0c1b709fe057b
ghollisjr <ghollisjr@gmail.com>
2024-04-08 22:05:48 -0400

    Updated aggregation example

--- example/aggregation.lisp
+++ example/aggregation.lisp
@@ -4,15 +4,13 @@
 (defun example-aggregate ()
   "Example aggregation: sum x grouped by y"
   (let ((table (make-table '((1 2) (3 4) (5 4)) :field-names '("X" "Y"))))
-    (agg (with-agg gy ((sumx (agg-sum gy)))
-             (list gy (sumx))
-             ;; Could also do:
-             ;; (list gy sumx)
-             ;; using the convenient symbol-macrolet
-             (x)
+    (agg (with-agg (y (tlambda (y) y)) ; group by y
+                   ((sumx (agg-sum)))
+                   (list y (sumx))
+                   ;; Could also do:
+                   ;; (list y sumx)
+                   ;; using the convenient symbol-macrolet
+                   (x)
            (sumx x))
          table
-         ;; group by y
-         :group-fn (tlambda (y)
-                     y)
          :field-names '("Y" "SUM(X)"))))
