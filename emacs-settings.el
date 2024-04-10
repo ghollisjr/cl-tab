1cfacef9adb7900bab3d07cadc6e57a320515dc5
ghollisjr <ghollisjr@gmail.com>
2024-04-10 13:06:22 -0400

    Updated emacs settings

--- emacs-settings.el
+++ emacs-settings.el
@@ -2,4 +2,26 @@
 ;;; add macro highlighting:
 (font-lock-add-keywords
  'lisp-mode
- '(("dotable" . font-lock-keyword-face)))
+ '(("tlambda" . font-lock-keyword-face)
+   ("tlambda*" . font-lock-keyword-face)))
+
+;; add tlambda & tlambda* pretty symbols
+(add-hook 'lisp-mode-hook
+          '(lambda ()
+             (push (cons "tlambda"
+                         (list ?t '(Br . Bl) ?λ))
+                   prettify-symbols-alist)
+             (push (cons "tlambda*"
+                         (list ?t '(Br . Bl) ?λ '(Br . Bl) ?*))
+                   prettify-symbols-alist)))
+
+;; same for slime repl
+(add-hook 'slime-repl-mode-hook
+          '(lambda ()
+             (prettify-symbols-mode 1)
+             (push (cons "tlambda"
+                         (list ?t '(Br . Bl) ?λ))
+                   prettify-symbols-alist)
+             (push (cons "tlambda*"
+                         (list ?t '(Br . Bl) ?λ '(Br . Bl) ?*))
+                   prettify-symbols-alist)))
